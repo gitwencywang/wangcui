fef64c3d9767fd89b958caf0c0382821d99b3817
王翠 <18817655087@163.com>
2018-05-16 15:55:18 +0800

    why

---
+++ src/getapost/get2.java
@@ -0,0 +1,61 @@
+package getapost;
+
+import java.util.List;
+import java.util.Map;
+
+import java.io.*;
+import java.net.*;
+
+public class get2 {
+    public static void main(String[] args){
+        String url="https://g.alicdn.com/secdev/sufei_data/3.4.1/index.js";
+        System.out.println(get(url));
+    }
+    public static String get(String url) {
+        String result = "";
+
+        BufferedReader in = null;
+        try {
+
+            URL realUrl = new URL(url);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println("返回结果的键值对是："+key + "--->" + map.get(key));
+            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+}
+
---
+++ src/getapost/post2.java
@@ -0,0 +1,77 @@
+package getapost;
+import java.util.List;
+import java.util.Map;
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+public class post2 {
+    public static void main(String[] args){
+        String url="https://g.alicdn.com/secdev/sufei_data/3.4.1/index.js";
+        System.out.println(post(url,param,isproxy));
+    }
+    public static String post(String url, String param, boolean isproxy) {
+        OutputStreamWriter out = null;
+        BufferedReader in = null;
+        String result = "";
+        try {
+            URL realUrl = new URL(url);
+            HttpURLConnection conn = null;
+            if (isproxy) {// 使用代理模式
+                @SuppressWarnings("static-access")
+                Proxy proxy = new Proxy(Proxy.Type.DIRECT.HTTP,
+                        new InetSocketAddress(proxyHost, proxyPort));
+                conn = (HttpURLConnection) realUrl.openConnection(proxy);
+            } else {
+                conn = (HttpURLConnection) realUrl.openConnection();
+            }
+            // 打开和URL之间的连接
+
+            // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestMethod("POST"); // POST方法
+
+            // 设置通用的请求属性
+
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            conn.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded");
+
+            conn.connect();
+
+            // 获取URLConnection对象对应的输出流
+            out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+            // 发送请求参数
+            out.write(param);
+            // flush输出流的缓冲
+            out.flush();
+            // 定义BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送 POST 请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输出流、输入流
+        finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
